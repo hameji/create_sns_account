23d5142f987d17297e0037ab11b810a5c0190bbb
hameji <hameji@gmail.com>
2021-04-12 03:26:21 -0400

    user_infoのクラス作成

--- model/user/user_info.py
+++ model/user/user_info.py
@@ -2,7 +2,9 @@
 # -*- coding: utf-8 -*-
 
 from dataclasses import dataclass
-import date
+from datetime import date
+from dateutil.relativedelta import relativedelta
+import random
 
 @dataclass
 class UserInfo(object):
@@ -13,3 +15,14 @@ class UserInfo(object):
     birthday: date
     age: int
     sex: int
+
+    def __init__(self, last_name: str, first_name: str, e_mail: str, start_age: int, end_age: int):
+        self.lastname = last_name
+        self.firstname = first_name
+        self.email = e_mail
+        self.passwd = ""
+        # date(YYYY, MM, dd)
+        self.birthday = date.today()
+        delta = relativedelta(self.birthday, date.today())
+        self.age = delta.years
+        self.sex = random.randomint(0, 1)
