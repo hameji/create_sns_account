4c33866cfac9464cb8a20f5bc1a45885aa26e2e3
hameji <hameji@gmail.com>
2021-04-11 21:57:57 -0400

    main.jsも作成

---
+++ view/main.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+});
+
+const exec_btn = document.querySelector("#exec-btn");
+exec_btn.addEventListener("click", () => {
+	console.log("click!");
+});
