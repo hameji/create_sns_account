5f2962b35dfc1e49db8cd8d25591b22ce95b07fe
hameji <hameji@gmail.com>
2021-04-12 20:28:42 -0400

    user_error作成

---
+++ model/error/user_error.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from model.error.chrome_error import Error
+
+class NameDivisionError(Error):
+    """Raised when the name element was only one"""
+    pass
