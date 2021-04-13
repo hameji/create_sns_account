d6dfaf31b5b27adcc6dd042eba999648a1ac943c
hameji <hameji@gmail.com>
2021-04-13 00:08:05 -0400

    stringExtensionのバグを修正

--- model/extension/string_extension.py
+++ model/extension/string_extension.py
@@ -4,11 +4,10 @@
 import string
 import secrets
 
-class String(str):
+class StringExtension(str):
     def random(self, digits: int):
         chars = string.ascii_uppercase + string.ascii_lowercase + string.digits
         # 記号を含める場合
         # chars += '%&$#()'
         random_str: str = ''.join(secrets.choice(chars) for x in range(digits))
-        self = super().__new__(self, random_str)
-        return self
+        return random_str
